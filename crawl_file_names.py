dc95eb1f3c8fc8b8363bac8b9ad8ec24ed53f9a4
Tobias Roth <toebgen@gmail.com>
2019-05-17 13:28:29 +0200

    Feat: Crawl path and save file name dictionary.

--- crawl_file_names.py
+++ crawl_file_names.py
@@ -3,6 +3,7 @@ import datetime
 import sys
 import os
 
+import json
 from tqdm import tqdm
 
 
@@ -11,9 +12,11 @@ def crawl_file_names(paths):
     dotfiles_full_paths = []
     file_names = []
     full_paths = []
+    file_names_dict = {}  # Store full path(s) for every file name
+
     for path in paths:
-        print('Crawling through the path...')
-        for dirpath, dirnames, filenames in tqdm(os.walk(path), desc='os walk'):
+        print('Crawling through the path...', path, '\n')
+        for dirpath, _, filenames in tqdm(os.walk(path), desc='os walk'):
             for filename in filenames:
                 full_path = os.path.join(dirpath, filename)
                 try:
@@ -23,16 +26,29 @@ def crawl_file_names(paths):
                 except (OSError,):
                     # not accessible (permissions, etc) - pass on
                     continue
+                
                 if filename.startswith('.'):
                     dotfiles.append(filename)
                     dotfiles_full_paths.append(full_path)
                 else:
                     file_names.append(filename)
                     full_paths.append(full_path)
+                    if filename not in file_names_dict:
+                        file_names_dict[filename] = [full_path]
+                    else:
+                        # print(file_names_dict[filename])
+                        file_names_dict[filename].append(full_path)
+                    number_of_entries = len(file_names_dict[filename])
+                    if number_of_entries > 1:
+                        # print('\nfile_names_dict[filename]:', file_names_dict[filename])
+                        # print('len(file_names_dict[filename]):', len(file_names_dict[filename]))
+                        # print('Found', number_of_entries, 'duplicates for file', filename)
+                        pass
 
         # Figure out duplicates
         unique_file_names = set(file_names)
-
+        duplicate_file_names_dict = {k: v for k, v in file_names_dict.items() if len(v)>1}
+        
         # Combine in one dictionary
         results = {
             'file_names': file_names,
@@ -40,27 +56,41 @@ def crawl_file_names(paths):
             'dotfiles': dotfiles,
             'dotfiles_full_paths': dotfiles_full_paths,
             'full_paths': full_paths,
+            'file_names_dict': file_names_dict,
+            'duplicate_file_names_dict': duplicate_file_names_dict,
         }
 
         # Output results
-        newpath = '{prefix}/crawl-files-{date:%Y-%m-%d-%H-%M-%S}/'.format(prefix=path, date=datetime.datetime.now())
-        if not os.path.exists(newpath):
-            os.makedirs(newpath)
-        for key, the_list in tqdm(results.items(), desc='results output'):
+        output_folder_name = 'crawlfiles-' + path.split('/')[-2]
+        outputpath = '{prefix}/{folder_name}-{date:%Y%m%d-%H%M%S}/'.format(
+            prefix='.',
+            folder_name=output_folder_name,
+            date=datetime.datetime.now()
+        )
+        if not os.path.exists(outputpath):
+            os.makedirs(outputpath)
+        for key, the_list in results.items():
             # Print quick statistic
-            print('Number of', key, ':', len(the_list))
+            print('Length of', key, ':', len(the_list))
 
             # Write list to files for further processing
-            with open(newpath + key + '.txt', 'w') as f:
+            with open(outputpath + key + '.txt', 'w') as f:
                 # print('Writing', key, 'to file...')
-                for item in the_list:
-                    f.write("%s\n" % item)
+                if isinstance(the_list, dict):
+                    # dump dict as json
+                    f.write(json.dumps(the_list, indent=4))
+                else:
+                    for item in the_list:
+                        f.write("%s\n" % item)
         
-        print('Finished!')
+        print('\n... Finished path', path, '!')
+    
+    print('\n... Done with all', len(paths), 'given paths!')
+    return duplicate_file_names_dict
                 
 
 if sys.argv[1:]:
-    crawl_file_names(sys.argv[1:])
+    duplicate_file_names_dict = crawl_file_names(sys.argv[1:])
 else:
     print("Please pass the paths to check as parameters to the script")
 
