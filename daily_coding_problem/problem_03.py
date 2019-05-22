e95ec6a94888e9c8a33426d814d9a6cfb0037e17
Tobias Roth <toebgen@gmail.com>
2019-05-22 15:59:18 +0200

    Add: Daily Coding problem 3, deserializing part.

--- daily_coding_problem/problem_03.py
+++ daily_coding_problem/problem_03.py
@@ -21,13 +21,17 @@ assert deserialize(serialize(node)).left.left.val == 'left.left'
 
 
 class Node:
-    def __init__(self, val, left=None, right=None):
+    def __init__(self, val=None, left=None, right=None, parent=None):
         self.val = val
         self.left = left
         self.right = right
+        self.parent = parent
     
     def is_leaf(self):
         return (self.left == None and self.right == None)
+    
+    def is_root(self):
+        return self.parent == None
 
 
 def serialize(node):
@@ -48,16 +52,46 @@ def deserialize(serialized_str):
     tokens = [token for token in serialized_str.split(' ') if token]
     if len(tokens)<1:
         return None
-    pass
+    print(tokens)
+    # root = Node(tokens[0])
+    # parent_node = root
+    node, previous_token = None, None
+    for token in tokens:
+        if token == '<':
+            previous_token = '<'
+        elif token == '>':
+            previous_token = '>'
+            if node.parent:
+                node = node.parent
+        elif token == ',':
+            previous_token = ','
+            if node.parent:
+                node = node.parent
+        else:
+            node = Node(val=token, parent=node)
+            if previous_token == '<':
+                node.parent.left = node
+            elif previous_token == ',':
+                node.parent.right = node
+    return node
 
 
 class TestProblem03(unittest.TestCase):
     def test(self):
         node = Node('root', Node('left', Node('left.left')), Node('right'))
-        node = Node('1', Node('2', Node('4'), Node('5')), Node('3', Node('7')))
-        print("serialize:", serialize(node))
-        # self.assertEqual(deserialize(serialize(node)).left.left.val, 'left.left')
+        self.assertEqual(deserialize(serialize(node)).left.left.val, 'left.left')
 
+        node = Node('1', Node('2', Node('4'), Node('5')), Node('3', Node('7')))
+        self.assertEqual(serialize(node), ' 1  <  2  <  4  ,  5  >  ,  3  <  7  ,  >  > ')
+        
+        deserialized_node = deserialize(serialize(node))
+        self.assertEqual(deserialized_node.val, '1')
+        self.assertEqual(deserialized_node.left.val, '2')
+        self.assertEqual(deserialized_node.left.left.val, '4')
+        self.assertEqual(deserialized_node.left.right.val, '5')
+        self.assertEqual(deserialized_node.right.val, '3')
+        self.assertEqual(deserialized_node.right.left.val, '7')
+        
 
 if __name__ == '__main__':
     unittest.main()
