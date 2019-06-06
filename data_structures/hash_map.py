ebdc4cb31847d4a14e98fd0118507e9f3e68e228
Tobias Roth <toebgen@gmail.com>
2019-06-06 17:42:58 +0200

    Feat: Handle collisions in HashMap implementation.

--- data_structures/hash_map.py
+++ data_structures/hash_map.py
@@ -4,7 +4,7 @@ import unittest
 class HashMap():
     def __init__(self, *args, **kwargs):
         # Instantiate initial fixed size array
-        self.array_length = 64
+        self.array_length = 32
         self.array = [None] * self.array_length
         self.num_saved_elements = 0
 
@@ -12,23 +12,32 @@ class HashMap():
     
 
     def key2index(self, key):
+        """ Use simple hash function key->index for now """
         return key % self.array_length
 
 
     def insert(self, key, value):
-        """ insert value at key to the map """
+        """
+        Insert value at corresponding index to key to the map.
+        Always save the given key as well, in order to be able to resolve collisions.
+        """
+
         index = self.key2index(key)
         if self.array[index] == None:
-            self.array[index] = value
+            self.array[index] = [(key, value)]
         else:
-            self.array[index].append(value)
+            self.array[index].append((key, value))
         self.num_saved_elements += 1
     
 
     def __getitem__(self, key):
         """ [key] operator, return value """
         index = self.key2index(key)
-        return self.array[index]
+        bucket = self.array[index]
+        for (_key, _value) in bucket:
+            if _key == key:
+                return _value
+        raise KeyError('Key not found in HashMap!')
     
 
     def fill_ratio(self):
@@ -41,10 +50,11 @@ class TestHashMap(unittest.TestCase):
 
         get_n_test_cases = lambda n : [(i, 'val'+str(i)) for i in range(n)]
         self.test_cases = get_n_test_cases(10)
-        # self.test_cases.append((100, 'val100'))
-        # self.test_cases.append((1000, 'val1000'))
-        # self.test_cases.append((111111, 'val11111'))
-        print(self.test_cases)
+
+        # The following cases will cause collisions
+        self.test_cases.append((100, 'val100'))
+        self.test_cases.append((1000, 'val1000'))
+        self.test_cases.append((111111, 'val11111'))
     
     
     def testInsertAndAccessSingle(self):
