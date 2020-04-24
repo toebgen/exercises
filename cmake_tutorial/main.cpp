39a98577cfc973aa09ec5d7524ad8a7b32f3f7be
Tobias Roth <toebgen@gmail.com>
2020-04-24 15:47:50 +0200

    Add boost library dependency.

--- cmake_tutorial/main.cpp
+++ cmake_tutorial/main.cpp
@@ -2,8 +2,10 @@
 #define CMAKEMACROSAMPLE "NO SYSTEM NAME"
 #endif
 
-#include "lib/math/operations.hpp"
+#include <boost/random.hpp>
 #include <iostream>
+
+#include "lib/math/operations.hpp"
 
 int main() {
   std::cout << "Hello CMake!"
@@ -12,4 +14,21 @@ int main() {
   math::operations op;
   int sum = op.sum(3, 4);
   std::cout << "Sum of 3+4: " << sum << std::endl;
+
+  // Boost random example
+  boost::mt19937 rng;
+  double mean = 2.4;
+  double std = 1.97;
+  auto normal_distribution =
+      boost::random::normal_distribution<double>(mean, std);
+
+  boost::variate_generator<boost::mt19937 &, boost::normal_distribution<>>
+      random_generator(rng, normal_distribution);
+
+  for (int i = 0; i < 2; ++i) {
+    auto rand_val = random_generator();
+    std::cout << "Random value " << i + 1 << ": " << rand_val << std::endl;
+  }
+
+  return 0;
 }
