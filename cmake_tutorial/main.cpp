483960fc8c19d043c6a1c7f9b1a153d1e302ea8a
Tobias Roth <toebgen@gmail.com>
2020-04-24 10:28:16 +0200

    Move CMake stuff into build folder. Rename executable. Add CMake Macro example. Add binary into build/bin/ folder. Disable in-source build.

--- cmake_tutorial/main.cpp
+++ cmake_tutorial/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 
-auto sum(int a, int b){
-	return a+b;
-}
+#ifndef CMAKEMACROSAMPLE
+#define CMAKEMACROSAMPLE "NO SYSTEM NAME"
+#endif
+
+auto sum(int a, int b) { return a + b; }
 
 int main() {
-	std::cout << "Hello CMake!" << std::endl;
-	std::cout << "Sum of 3+4: " << sum(3, 4) << std::endl;
+  std::cout << "Hello CMake!"
+            << " We are on a " << CMAKEMACROSAMPLE << " system!" << std::endl;
+
+  std::cout << "Sum of 3+4: " << sum(3, 4) << std::endl;
 }
